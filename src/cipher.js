3d431ca524a8160fd9f444d75571b8a36550efa2
Jessica Poves <povesjessica2@gmail.com>
2018-06-01 00:59:40 -0500

    Cifra y descifra en mayusculas y minusculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,50 +1,43 @@
-function encode() {
-    /*uso let porque:
-     uso querySelector porque
-    JavaScript String -- toLocalUpperCase- convierte una cadena en letras mayúsculas 
-       */
-    let decryptedtext = document.querySelector('#decryptedtext').value.toLocaleUpperCase();
-    let encryptedtext = document.querySelector('#encryptedtext');
-    let offset = document.querySelector('#offset');
-    let cipherArray = new Array();
-    [...decryptedtext].forEach(char => {
-        //console.log(char.charCodeAt())
-        if (char.charCodeAt() === 32) {
-            cipherArray.push(32);
-        } else {
-            charNumber = char.charCodeAt() - 65;
-            //console.log(charNumber)
-            cipherChar = (charNumber + parseInt(offset.value)) % 26;
-            //console.log(cipherChar)
-            cipherArray.push(cipherChar + 65);
+window.cipher = {  
+    encode: (offset, string) => {
+        let cifrado = '';
+        offset = offset % 26;
+        for(let i=0; i < string.length; i++){
+          let abcAscii = string.charCodeAt(i);
+          let formulaCc;
+          if((abcAscii >=65) && (abcAscii <=90)){
+            formulaCc = (abcAscii -65+offset)%26+65;
+          } else if((abcAscii >=97) && (abcAscii <=122)){
+            formulaCc = (abcAscii -97+offset)%26+97;
+          } else{
+            formulaCc = abcAscii;
+          }
+          cifrado += String.fromCharCode(formulaCc);
         }
-    })
-    /*Uso String.fromCharcode*/
-    document.getElementById("result").value = String.fromCharCode(...cipherArray);
-    //console.log(String.fromCharCode(...cipherArray))
-}
-
-function decode() {
-    // cambiè let x VAR Y # va a cambiar el texto que luego serà mostrado en el contenido del recuadro //
-    let decryptedtext = document.querySelector('#decryptedtext');
-    let encryptedtext = document.querySelector('#encryptedtext').value.toLocaleUpperCase();
-    let offset = document.querySelector('#offset');
-    let cipherArray = new Array();
-    //let cipherArray = parseInt(space);//
-    [...encryptedtext].forEach(char => {
-        //console.log(char.charCodeAt())
-        if (char.charCodeAt() === 32) {
-            cipherArray.push(32);
-        } else {
-            charNumber = char.charCodeAt() - 65;
-            //console.log(charNumber)
-            cipherChar = (charNumber - parseInt(offset.value));
-            //console.log(cipherChar)
-            if (cipherChar <= 26 || cipherChar > 0) cipherChar = cipherChar % 26
-            cipherChar = cipherChar % 26
-            cipherArray.push(cipherChar + 65);
+        return cifrado;
+      },
+    
+      decode: (offset, string) =>{
+        let descifrado = '';
+        offset = offset % 26;
+        for(let i=0; i < string.length; i++){
+          let abcAscii = string.charCodeAt(i);
+          let formulaCc;
+          if((abcAscii >=65) && (abcAscii <=90)){
+            formulaCc = (abcAscii -65-offset+26)%26+65;
+          } else if((abcAscii >=97) && (abcAscii <=122)){
+            formulaCc = (abcAscii -97-offset+26)%26+97;
+          }else{
+            formulaCc = abcAscii;
+          }
+          descifrado += String.fromCharCode(formulaCc);
         }
-    })
-    document.getElementById("result").value = String.fromCharCode(...cipherArray);
-    //console.log(String.fromCharCode(...cipherArray))
-}
+        return descifrado;
+      },
+      
+      createCipherWithOffset: (offset, string) =>{
+        return encode;
+        return decode;
+    }
+  };
+      
--- src/scripts.js
+++
@@ -1,7 +0,0 @@
-function clearBoxes() {
-    document.querySelector('#decryptedtext').value = "";
-    document.querySelector('#encryptedtext').value = "";
-    document.getElementById("result").value = "";
-    document.getElementById("offset").value = 0;
-}
-
